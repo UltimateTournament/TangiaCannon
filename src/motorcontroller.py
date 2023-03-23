93c1f94dea36a08031884fd02b0e8c039e238c54
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-03-22 21:34:04 -0400

    add motor package
    add pin registration
    
    check capacity sensor

--- src/motorcontroller.py
+++ src/motorcontroller.py
@@ -1,11 +1,22 @@
-import board
+import digitalio, pwmio, time
+from adafruit_motor import servo
 
 class MotorController:
   def __init__(self, revServoPin, fwdServoPin, motorPin, capSensPin) -> None:
-    self.revServoPin = revServoPin
-    self.fwdServoPin = fwdServoPin
-    self.motorPin = motorPin
-    self.capSensPin = capSensPin
+    self.revServoPin = pwmio.PWMOut(revServoPin, duty_cycle=2 ** 15, frequency=50)
+    self.revServo = servo.Servo(revServoPin)
+
+    self.fwdServoPin = pwmio.PWMOut(fwdServoPin, duty_cycle=2 ** 15, frequency=50)
+    self.fwdServo = servo.Servo(fwdServoPin)
+    self.revServo.angle = 180
+    self.fwdServo.angle = 10
+
+    self.motor = digitalio.DigitalInOut(motorPin)
+    self.motor.direction = digitalio.Direction.OUTPUT
+
+    self.capSens = digitalio.DigitalInOut(motorPin)
+    self.capSens.direction = digitalio.Direction.INPUT
+    self.capSens.pull = digitalio.Pull.UP
     # Feather32 S2
     # reverseServo = board.D6
     # forwardServo = board.D5
@@ -25,4 +36,4 @@ class MotorController:
 
   def checkCapacity():
     """Checks for whether there is at least one shot remaining determined by the capacity sensor"""
-    return True
+    return self.capSense.value
