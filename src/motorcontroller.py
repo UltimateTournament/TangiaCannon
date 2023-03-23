517ef1376b3a05d9e6f028a219c8bb76510dc7be
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-03-23 13:14:23 -0400

    tested working motor controller

--- src/motorcontroller.py
+++ src/motorcontroller.py
@@ -3,8 +3,8 @@ from adafruit_motor import servo
 
 boardToPins = {
   "unexpectedmaker_feathers2": {
-    "revServoPin": board.D6,
-    "fwdServoPin": board.D5,
+    "revServoPin": board.D5,
+    "fwdServoPin": board.D6,
     "motorPin": board.D9,
     "capSensPin": board.D12
   },
@@ -18,12 +18,13 @@ boardToPins = {
 
 class MotorController:
   def __init__(self) -> None:
-    self.revServoPin = pwmio.PWMOut(boardToPins[board.board_id]["revServoPin"], duty_cycle=2 ** 15, frequency=50)
-    self.revServo = servo.Servo(boardToPins[board.board_id]["revServoPin"])
+    print("detected board {}".format(board.board_id))
+    self.revServoPin = pwmio.PWMOut(boardToPins[board.board_id]["revServoPin"], duty_cycle=2**15, frequency=50)
+    self.revServo = servo.Servo(self.revServoPin)
 
-    self.fwdServoPin = pwmio.PWMOut(boardToPins[board.board_id]["fwdServoPin"], duty_cycle=2 ** 15, frequency=50)
-    self.fwdServo = servo.Servo(boardToPins[board.board_id]["fwdServoPin"])
-    self.revServo.angle = 180
+    self.fwdServoPin = pwmio.PWMOut(boardToPins[board.board_id]["fwdServoPin"], duty_cycle=2**15, frequency=50)
+    self.fwdServo = servo.Servo(self.fwdServoPin)
+    self.revServo.angle = 170
     self.fwdServo.angle = 10
 
     self.motor = digitalio.DigitalInOut(boardToPins[board.board_id]["motorPin"])
@@ -34,17 +35,34 @@ class MotorController:
     self.capSens.pull = digitalio.Pull.UP
     self.resetState()
 
-  def boltBack():
+  def setAngle(self, fwd, rev):
+    self.revServo.angle = rev
+    self.fwdServo.angle = fwd
+
+  def boltBack(self):
     print("moving bolt back")
+    self.revServo.angle = 170
+    self.fwdServo.angle = 10
+    time.sleep(0.6)
+    self.turnOffServos()
 
-  def boltForward():
+  def boltForward(self):
     print("moving bolt forward")
+    self.revServo.angle = 0
+    self.fwdServo.angle = 180
+    time.sleep(0.6)
+    self.turnOffServos()
 
-  def motorUp():
+  def motorUp(self):
     print("spinning motor up")
 
-  def motorDown():
+  def motorDown(self):
     print("spinning motor up")
+
+  def turnOffServos(self):
+    print("turning off servos")
+    self.revServoPin.duty_cycle = 0
+    self.fwdServoPin.duty_cycle = 0
 
   def hasCapacity(self):
     """Checks for whether there is at least one shot remaining determined by the capacity sensor"""
@@ -56,18 +74,17 @@ class MotorController:
 
   def resetState(self):
     print("resetting state")
-    self.boltBack()
     self.motor.value = False
-    time.sleep(0.6) # time for servos to move
+    self.boltBack()
 
 
   def ShootSingleSequence(self):
     print("shooting one")
     if not self.hasCapacity():
       return
-    self.boltBack()
     self.motor.value = True
-    time.sleep(3)
+    self.boltBack()
+    time.sleep(2.4)
     self.boltForward()
     time.sleep(1)
     self.resetState()
@@ -76,19 +93,19 @@ class MotorController:
     print("shooting all")
     if not self.hasCapacity():
       return
-    self.boltBack()
     self.motor.value = True
+    self.boltBack()
     time.sleep(2)
 
     while self.hasCapacity():
       time.sleep(1)
       self.boltForward()
-      time.sleep(0.6)
       self.boltBack()
     self.resetState()
 
   def FakeShootSequence(self):
     print("fake shooting sequence")
-    self.prepareToShoot()
-    time.sleep(1) # sleep an extra second
+    self.boltBack()
+    self.motor.value = True
+    time.sleep(3)
     self.resetState()
